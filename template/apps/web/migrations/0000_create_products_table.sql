646d9f2d7c7d814679b6f28d19a5ef01815f3431
Muhammad Tsany Qudsi <phlacx@hotmail.com>
2026-07-22 18:44:56 +0700

    feat: consolidate and restructure database migrations for products and authentication

--- template/apps/web/migrations/0000_create_products_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS products (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  description TEXT NOT NULL,
-  price_cents INTEGER NOT NULL,
-  image_key TEXT NOT NULL,
-  created_at TEXT NOT NULL DEFAULT (datetime('now'))
-);
--- template/apps/web/migrations/0002_add_auth_roles.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE user ADD COLUMN role TEXT NOT NULL DEFAULT 'customer'
-  CHECK (role IN ('admin', 'staff', 'customer'));
-ALTER TABLE user ADD COLUMN banned INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE user ADD COLUMN banReason TEXT;
-ALTER TABLE user ADD COLUMN banExpires TEXT;
-
-ALTER TABLE session ADD COLUMN impersonatedBy TEXT;
-
-CREATE INDEX IF NOT EXISTS user_role_idx ON user(role);
